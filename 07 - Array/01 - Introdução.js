f1d3b47a45f3f2ad59204a1635e46ed365833dc2
Vinicius Vieira <vinicius.vdes@gmail.com>
2019-11-09 16:19:23 -0300

    Adicionando novas pastas para organizar o repositório.

---
+++ 07 - Array/01 - Introdução.js
@@ -0,0 +1,25 @@
+// Arrays em JavaScript começam com indíce 0.
+primeiro_array = ['Vinicius', 'Ricardo', 'Jess', 'Evelyn']
+
+// Adicionando um novo elemento à lista em posição aleatória
+primeiro_array[4] = 'Bianca'
+
+// Adicionando na última posição:
+primeiro_array.push('Vicente')
+
+// Printando o index e o conteúdo de cada elemento...
+for (let i in primeiro_array){
+    console.log(i, primeiro_array[i])
+}
+
+// Imprimindo o lenght
+console.log(`Comprimento do array: ${primeiro_array.length}`)
+
+// Removendo um elemento
+delete primeiro_array[1]
+console.log(primeiro_array[1]) // Posição vazia. Preenchida com undefined.
+console.log(primeiro_array)
+
+// função splice -> array.splice(indice de onde ele começa a trabalhar, excluí os n itens depois desse índice, elementos a serem adicionados)
+primeiro_array.splice(1,1,'Ai dento')
+console.log(primeiro_array)
